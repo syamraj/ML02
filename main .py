7ba42dbfb96844a53a860d9ad8cd0d3fdb2dc0f6
pegahsafaie <pegahsafaie@gmail.com>
2016-11-27 10:22:27 +0100

    train model

--- main .py
+++ main .py
@@ -1,11 +1,11 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn import linear_model
-import datetime
 from calculatingMSF import MSF
 from common import common
 
 raw_df = pd.read_hdf('data.h5', 'raw')
-labels_df = pd.read_hdf('data.h5', 'labels')
+
 
 feature_df = pd.DataFrame()
 for subject_id, subject_df in raw_df.groupby(raw_df.subject):
@@ -19,58 +19,28 @@ date = feature_df.Test_time.apply(lambda x: x.date())
 feature_df['time_of_day'] = h + (m / 60.0)
 feature_df['date'] = date
 
-date = labels_df.Time.apply(lambda x: x.date())
-labels_df['date'] = date
 
-participant_work_list = pd.DataFrame()
-for subject_id, subject_df in labels_df.groupby(labels_df.Participant_ID):
-    for date_dy, date_df in subject_df.groupby(subject_df.date):
-        participant_work_list = participant_work_list.append(pd.Series(
-            {
-                # normally for determining free or work date subject_ID dosnt matte,
-                # but sometimes the work day and free day of different people differ
-                'Subject_id': subject_id,
-                'Date': date_dy,
-                'Workday': date_df.Workday.iloc[0],
+x_set = [[y] for y in feature_df.groupby(feature_df.Subject).q95_mean.mean().values]
+y_set = MSF.compute_MSF(feature_df)
 
-            }), ignore_index=True)
+y_train_set = y_set[:-2]
+y_test_set = y_set[-2:]
 
-MSF_participant_sleep_list = pd.DataFrame()
-for subject_id, subject_df in feature_df.groupby(feature_df.Subject):
-    for date_day, date_df in subject_df.groupby(subject_df.date):
+x_train_set = x_set[:-2]
+x_test_set = x_set[-2:]
 
-        SD = 0
-        Workday = 0
+regr = linear_model.LinearRegression()
+regr.fit(x_train_set, y_train_set)
+predict = regr.predict(x_test_set)
+print(regr.score(x_test_set, y_test_set))
 
-        if (len(MSF_participant_sleep_list) > 0 and
-                    len(MSF_participant_sleep_list.loc[MSF_participant_sleep_list.Subject_id == subject_id]) > 0):
+# Plot outputs
+# plt.scatter(x_test_set, y_test_set,  color='black')
+# plt.plot(x_test_set, regr.predict(x_test_set), color='blue',linewidth=3)
+#
+# plt.xticks(())
+# plt.yticks(())
+#
+# plt.show()
 
-            yesterday_SO = MSF_participant_sleep_list.loc[MSF_participant_sleep_list.Subject_id == subject_id].SO.iloc[
-                -1]
-            today_wakeup_time = min(date_df.Test_time)
-
-            if (yesterday_SO.date() == today_wakeup_time.date() - datetime.timedelta(days=1)):
-                dif = today_wakeup_time - yesterday_SO
-                SD = dif.seconds / (60)
-
-        workdayRow = participant_work_list.loc[(participant_work_list.Date == date_day) &
-                                               (participant_work_list.Subject_id == subject_id)]
-
-        if (len(workdayRow.Workday.values) > 0):
-            Workday = workdayRow.Workday.values[0]
-
-        i = max(date_df.Test_time).to_pydatetime()
-        MSF_participant_sleep_list = MSF_participant_sleep_list.append(pd.Series(
-            {
-                'Subject_id': subject_id,
-                'SO': max(date_df.Test_time),
-                'SO_Sec': int((i.hour * 3600 + i.minute * 60 + i.second)),  # seconds
-                'SD': SD,  # seconds
-                'Workday': Workday
-            }), ignore_index=True)
-
-MSF.compute_MSF(MSF_participant_sleep_list, verbose=False)
-
-# regr = linear_model.LinearRegression()
-# regr.fit(featture_train_set, y_train_set)
 # last step: comare the result of the model(e=circadian phase base on reaction time) and r=MSF circadian phase#################
